ee5f8f548be49814f99212d9f1c78aaf798b3431
Sk Imran Ali <108138765+skimranali2020@users.noreply.github.com>
2024-11-08 15:35:53 +0530

    Change Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <h2>YourBrand</h2>
+        <h2>MyBrand</h2>
       </div>
       <ul className="navbar-menu">
         <li><Link to='/'><FontAwesomeIcon icon={faPlane} /> Flights</Link></li>
